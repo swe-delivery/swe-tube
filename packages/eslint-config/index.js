f1041d5211086b00486874410202376514ae6ecf
Matthew Gibbons <matthew@southwestkoala.co.uk>
2023-03-01 11:24:11 +0000

    Created and configured common eslint configuration

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
+};
