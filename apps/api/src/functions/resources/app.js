309c89683766a6f2ae2344843634b46e5cc29536
Matthew Gibbons <matthew.gibbons@multiverse.io>
2023-03-04 07:20:08 +0000

    Added missing router middleware for deployed

--- apps/api/src/functions/resources/app.js
+++ apps/api/src/functions/resources/app.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env["NETLIFY_DEV"]) {
-  app.use("/.netlify/functions/resources", router);
+	app.use("/.netlify/functions/resources", router);
 } else {
-  app.use("/resources");
+	app.use("/resources", router);
 }
 
 module.exports = app;
